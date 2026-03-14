4c76d9e199ca28e314a81af02a5fafbde53f4569
yash112021 <yp9447005@gmail.com>
2026-03-14 18:48:43 +0530

    Praymid traingle

---
+++ Pattern.java
@@ -0,0 +1,26 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // for  print spaces befor
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            // half side
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            //  remainaing half part  
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
